b376710a15edb36553e1d57182ea4886ab37c6a9
Jaewook <eari100@naver.com>
2022-03-10 18:19:19 +0900

    [Java] 백준, 1463번 1로 만들기
    - dp(bottom up)

---
+++ Java/Baekjoon/Q1463.java
@@ -0,0 +1,27 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *@source: https://www.acmicpc.net/problem/1463
+ *@classfication: dp
+ **/
+public class Q1463 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        br.close();
+
+        int[] memoization = new int[X+1];
+
+        for(int i=2;i<=X;i++) {
+            memoization[i] = memoization[i-1]+1;
+            if(i%3==0) memoization[i] = Math.min(memoization[i], memoization[i/3]+1);
+            if(i%2==0) memoization[i] = Math.min(memoization[i], memoization[i/2]+1);
+        }
+
+        System.out.print(memoization[X]);
+    }
+}
