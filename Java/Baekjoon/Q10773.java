2000ab0bcf510e086d1d388d981ec6d830164346
Jaewook Choi <eari100@naver.com>
2022-03-13 00:13:11 +0900

    [Java] 백준, 10773번 제로
    - stack
    - stream 활용

---
+++ Java/Baekjoon/Q10773.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Q10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<K;i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if(x==0) stack.pop();
+            else stack.push(x);
+        }
+
+        System.out.print(stack.stream().mapToInt(s -> s).sum());
+        // System.out.print(stack.stream().reduce(0, Integer::sum));
+    }
+}
