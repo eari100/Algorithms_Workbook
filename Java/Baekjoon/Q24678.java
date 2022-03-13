706e97a69f0eafd349c6a944d35bcd5a5697f44b
Jaewook Choi <eari100@naver.com>
2022-03-13 15:02:13 +0900

    [Java] 백준,24678번 돌무더기 게임 1
    - game theory

---
+++ Java/Baekjoon/Q24678.java
@@ -0,0 +1,30 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ *@source: https://www.acmicpc.net/problem/24678
+ *@classfication: game theory
+ **/
+public class Q24678 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int x, y, z;
+        for (int T = Integer.parseInt(br.readLine()); T > 0; T--) {
+            st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            z = Integer.parseInt(st.nextToken());
+            if ((x % 2) + (y % 2) + (z % 2) <= 1)
+                sb.append('R').append('\n');
+            else
+                sb.append('B').append('\n');
+        }
+        System.out.print(sb);
+    }
+}
