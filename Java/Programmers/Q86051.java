1c9dd995724f11d82b6ac3dcfcec676f6eca8f1f
Jaewook Choi <eari100@naver.com>
2022-03-14 21:03:35 +0900

    [Java] 프로그래머스, 86051번 없는 숫자 더하기
    - binary search

---
+++ Java/Programmers/Q86051.java
@@ -0,0 +1,36 @@
+package Programmers;
+
+import java.util.Arrays;
+
+/**
+ *@source: https://programmers.co.kr/learn/courses/30/lessons/86051 (없는 숫자 더하기)
+ *@classfication: binary search
+ **/
+public class Q86051 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+
+        Arrays.sort(numbers);
+
+        for(int i=0;i<=9;i++)
+            if( !binarySearch(numbers, i) ) answer += i;
+
+        return answer;
+    }
+
+    private boolean binarySearch(int[] numbers, int x) {
+
+        int strIdx = 0;
+        int endIdx = numbers.length-1;
+        int mid;
+
+        while(strIdx <= endIdx) {
+            mid = (strIdx+endIdx)/2;
+            if(x == numbers[mid]) return true;
+            else if(x < numbers[mid]) endIdx = mid-1;
+            else if(x >  numbers[mid]) strIdx = mid+1;
+        }
+
+        return false;
+    }
+}
