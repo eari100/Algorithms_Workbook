277e4c7ffc912fb8c7eebc1379456cc7792485a6
Jaewook Choi <eari100@naver.com>
2022-03-18 23:32:19 +0900

    [Java] 백준, 11726번 2×n 타일링
    - dp

---
+++ Java/Baekjoon/Q11726.java
@@ -0,0 +1,24 @@
+package Baekjoon;
+
+import java.io.*;
+
+/**
+ *@source: https://www.acmicpc.net/problem/11726 (2×n 타일링)
+ *@classfication: dp
+ **/
+public class Q11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        int[] memo = new int[1001];
+        memo[1] = 1;
+        memo[2] = 2;
+
+        for(int i=3;i<=n;i++)
+            memo[i] = (memo[i-1] + memo[i-2])%10007;
+
+        System.out.print(memo[n]);
+    }
+}
