7f7b369c5f16793d1c44e98b771b4622aba91c48
Jaewook Choi <eari100@naver.com>
2022-03-27 01:04:09 +0900

    [Java] 백준, 9012번 괄호
    - stack

---
+++ Java/Baekjoon/Q9012.java
@@ -0,0 +1,30 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Q9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=Integer.parseInt(br.readLine());i>0;i--)
+            sb.append(process(br.readLine())).append("\n");
+        br.close();
+
+        System.out.print(sb);
+    }
+
+    private static String process(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if(c == '(') stack.push(c);
+            else if(stack.isEmpty()) return "NO";
+            else stack.pop();
+        }
+
+        if(stack.isEmpty()) return "YES";
+        else return "NO";
+    }
+}
