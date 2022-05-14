782186d446e659defe4b22d8358142aa5eecbc2c
Jaewook Choi <eari100@naver.com>
2022-05-14 12:32:48 +0900

    [Java] 백준, 1389 케빈 베이컨의 6단계 법칙
    - floyd warshall
    - 2차 배열의 Arrays.fill() 사용법 학습

---
+++ Java/Baekjoon/floydWarshall/Q1389.java
@@ -0,0 +1,69 @@
+package Baekjoon.floydWarshall;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q1389 {
+    static int[][] gragh;
+    static int INF = 100;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        gragh = new int[N][N];
+
+        for(int i[] : gragh)
+            Arrays.fill(i, INF);
+
+        for(int i=0;i<M;i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st2.nextToken()) - 1;
+            int to = Integer.parseInt(st2.nextToken()) - 1;
+
+            gragh[from][to] = gragh[to][from] = 1;
+        }
+        br.close();
+
+        floydWarshall();
+
+        System.out.print(minValIdx());
+    }
+
+    private static void floydWarshall() {
+        for(int k=0;k<gragh.length;k++) {
+            for(int i=0;i<gragh.length;i++) {
+                for(int j=0;j<gragh.length;j++) {
+                    gragh[i][j] = Math.min(gragh[i][j], gragh[i][k] + gragh[k][j]);
+                }
+            }
+        }
+    }
+
+    private static int minValIdx() {
+        int idx = 100;
+        int sum = 100;
+
+        for(int i=0;i<gragh.length;i++) {
+            int sumN = 0;
+
+            for(int j=0;j<gragh.length;j++) {
+                sumN += gragh[i][j];
+            }
+
+            if(sum > sumN) {
+                idx = i+1;
+                sum = sumN;
+            } else if(sum == sumN && idx > i+1) {
+                idx = i+1;
+                sum = sumN;
+            }
+        }
+
+        return idx;
+    }
+}
