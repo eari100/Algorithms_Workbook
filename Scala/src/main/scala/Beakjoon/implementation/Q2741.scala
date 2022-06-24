442c2f8e6e82f68cc004151580224d52f63b4940
Jaewook <eari100@naver.com>
2022-06-24 22:59:11 +0900

    [Scala] 백준, 2741. N 찍기
    - implementation

---
+++ Scala/src/main/scala/Beakjoon/implementation/Q2741.scala
@@ -0,0 +1,15 @@
+package Beakjoon.implementation
+
+import scala.io.StdIn.readInt
+
+/**
+ * @source: https://www.acmicpc.net/problem/2741 (N 찍기)
+ * @classfication: implementation
+ * @문제 푼 날짜 (자력으로 풂?)
+ *     1. 22.06.24 (O)
+ * */
+object Main {
+  def main(args: Array[String]): Unit = {
+    (1 to readInt).foreach(println)
+  }
+}
