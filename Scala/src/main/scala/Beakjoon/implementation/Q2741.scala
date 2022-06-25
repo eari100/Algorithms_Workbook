713e0e2f0954fd1fa65364f577dd4ebba44d1fc2
Jaewook <eari100@naver.com>
2022-06-25 14:48:52 +0900

    [Scala] object 명 변경

--- Scala/src/main/scala/Beakjoon/implementation/Q2741.scala
+++ Scala/src/main/scala/Beakjoon/implementation/Q2741.scala
@@ -8,7 +8,7 @@ import scala.io.StdIn.readInt
  * @문제 푼 날짜 (자력으로 풂?)
  *     1. 22.06.24 (O)
  * */
-object Main {
+object Q2741 {
   def main(args: Array[String]): Unit = {
     (1 to readInt).foreach(println)
   }
