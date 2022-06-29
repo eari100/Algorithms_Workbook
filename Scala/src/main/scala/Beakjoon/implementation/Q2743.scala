f26bf787dfa245945fc9ca8ee29b2c921f8e404c
Jaewook Choi <eari100@naver.com>
2022-06-29 20:21:37 +0900

    [Scala] 백준, 2743. 단어 길이 재기
    - implementation

---
+++ Scala/src/main/scala/Beakjoon/implementation/Q2743.scala
@@ -0,0 +1,15 @@
+package Beakjoon.implementation
+
+import scala.io.StdIn
+
+/**
+ * @source: https://www.acmicpc.net/problem/2743 (단어 길이 재기)
+ * @classfication: implementation
+ * @문제 푼 날짜 (자력으로 풂?)
+ *     1. 22.06.29 (O)
+ * */
+object Q2743 {
+  def main(args: Array[String]): Unit = {
+    print(StdIn.readLine().length)
+  }
+}
