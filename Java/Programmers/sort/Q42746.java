5d35a8ebdfb3b4d001b023fae96ed5f7bf87ddba
Jaewook <eari100@naver.com>
2022-07-06 23:42:50 +0900

    [Java] programmers, 42746. 가장 큰 수
    - sort
    - 2022.07.06 재시도 실패

--- Java/Programmers/sort/Q42746.java
+++ Java/Programmers/sort/Q42746.java
@@ -3,8 +3,11 @@ package Programmers.sort;
 import java.util.Arrays;
 
 /**
- *@source: https://programmers.co.kr/learn/courses/30/lessons/42746 (가장 큰 수)
- *@classfication: sort
+ * @source: https://programmers.co.kr/learn/courses/30/lessons/42746 (가장 큰 수)
+ * @classfication: sort
+ * @문제 푼 날짜 (자력으로 풂?)
+ * 1. 기록이 안됨 (X)
+ * 2. 22.07.06 (X)
  **/
 public class Q42746 {
     public String solution(int[] numbers) {
@@ -13,7 +16,7 @@ public class Q42746 {
         for(int i=0;i<numbers.length;i++)
             convertToStr[i] = Integer.toString(numbers[i]);
 
-        Arrays.sort(convertToStr, (s1, s2) -> (s1+s2).compareTo(s2+s1));
+        Arrays.sort(convertToStr, (s1, s2) -> s2.concat(s1).compareTo(s1.concat(s2)));
 
         if(convertToStr.equals("0")) return "0";
 
