3c15094aad021383b149137410b850f337b44852
Jaewook <eari100@naver.com>
2022-07-15 13:14:28 +0900

    [Java] Programmers, 12979. 기지국 설치
    - 업솔빙 실패

--- Java/Programmers/greedy/Q12979.java
+++ Java/Programmers/greedy/Q12979.java
@@ -6,20 +6,20 @@ package Programmers.greedy;
  * @reference: https://small-stap.tistory.com/81
  * @문제 푼 날짜 (혼자 힘으로 풂?)
  * 1. 22.07.06 (X)
+ * 2. 22.07.15 (X)
  **/
 public class Q12979 {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-
         int str = 1;
-        int x = 2*w+1;
+        int radioRange = 2*w+1;
 
         for(int station : stations) {
             if(str < station-w) {
                 int len = station-w-str;
 
-                answer += len / x;
-                if(len % x > 0) ++answer;
+                answer += len / radioRange;
+                if(len % radioRange > 0) ++answer;
             }
 
             str = station+w+1;
@@ -28,8 +28,8 @@ public class Q12979 {
         if(stations[stations.length-1]+w < n) {
             int len = n+1-str;
 
-            answer += len / x;
-            if(len % x > 0) ++answer;
+            answer += len / radioRange;
+            if(len % radioRange > 0) ++answer;
         }
 
         return answer;
