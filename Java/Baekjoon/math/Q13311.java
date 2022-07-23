8db00d2f0f2fc26e386540effcee64340e7d2542
Jaewook Choi <eari100@naver.com>
2022-07-24 00:10:18 +0900

    [Java] Baekjoon, 13311. 행운의 편지
    - math
    - number theory

---
+++ Java/Baekjoon/math/Q13311.java
@@ -0,0 +1,13 @@
+package Baekjoon.math;
+
+/**
+ * @source: https://www.acmicpc.net/problem/13311 (행운의 편지)
+ * @classfication: math, number theory
+ * @문제 푼 날짜 (혼자 힘으로 풂?)
+ * 1. 22.07.24 (X)
+ **/
+public class Q13311 {
+    public static void main(String[] args) {
+        System.out.print(-1);
+    }
+}
