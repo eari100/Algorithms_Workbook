1fd5cd3c7088925fdd8ea503ac4b1acc1b6cd460
Jaewook Choi <eari100@naver.com>
2022-07-29 00:15:11 +0900

    [Java] LeetCode, 242. Valid Anagram
    - implementation

--- Java/leetcode/implementation/Q242.java
+++ Java/leetcode/implementation/Q242.java
@@ -4,14 +4,13 @@ package leetcode.implementation;
  * @source: https://leetcode.com/problems/valid-anagram/ (242. Valid Anagram)
  * @classfication: implementation
  * @문제 푼 날짜 (자력으로 풂?)
- * 1. 22.07.28 (O)
+ * 1. 22.07.28 (O, Runtime: 7 ms, faster than 53.66%, Memory Usage: 43 MB, less than 84.83%)
  **/
 public class Q242 {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
 
         int[] arr = new int[26];
-
 
         for(int i=0;i<s.length();i++) {
             ++arr[s.charAt(i) - 97];
