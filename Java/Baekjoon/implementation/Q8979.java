9c1a78b86ea34816537ba5490c69e9b8a0f2a443
Jaewook <eari100@naver.com>
2023-03-03 22:58:16 +0900

    [Java] Baekjoon, 8979. 올림픽, implementation

--- Java/Baekjoon/implementation/Q8979.java
+++ Java/Baekjoon/implementation/Q8979.java
@@ -3,13 +3,12 @@ package Baekjoon.implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
  * @source: https://www.acmicpc.net/problem/8979 (올림픽)
  * @classfication: implementation
- * @문제 푼 날짜 (자력으로 풂?): 23.03.03 (X, 시간 초과)
+ * @문제 푼 날짜 (자력으로 풂?): 23.03.03 (X)
  **/
 public class Q8979 {
     public static void main(String[] args) throws IOException {
@@ -20,8 +19,8 @@ public class Q8979 {
         int K = Integer.parseInt(st.nextToken());
 
         Nation[] nations = new Nation[N];
-
-        int tmpRank = 1;
+        Nation targetNation = null;
+        int kRank = 1;
 
         for(int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
@@ -31,36 +30,33 @@ public class Q8979 {
             int bronzeCnt = Integer.parseInt(st.nextToken());
 
             nations[i] = new Nation(nationNo, goldCnt, silverCnt, bronzeCnt);
+
+            if(K == nationNo) targetNation = nations[i];
         }
 
         br.close();
 
-        Arrays.sort(nations, (nation1, nation2) -> {
-           if(nation1.getGoldCnt() == nation2.getGoldCnt()) {
-               if(nation1.getSilverCnt() == nation2.getSilverCnt()) {
-                   if(nation1.getBronzeCnt() == nation2.getBronzeCnt()) {
-                       return 0;
-                   } else {
-                       return nation2.getBronzeCnt() - nation1.getBronzeCnt();
-                   }
-               } else {
-                   return nation2.getSilverCnt() - nation1.getSilverCnt();
-               }
-           } else {
-               return nation2.getGoldCnt() - nation1.goldCnt;
-           }
-        });
+        int targetGoldCnt = targetNation.getGoldCnt();
+        int targetSilverCnt = targetNation.getSilverCnt();
+        int targetBronzeCnt = targetNation.getBronzeCnt();
 
-        for(int i=1;i<nations.length;i++) {
-            Nation nation1 = nations[i-1];
-            Nation nation2 = nations[i];
 
-            if(!nation1.같은_등수인가(nation2)) ++tmpRank;
+        for(int i=0;i<nations.length;i++) {
+            Nation nation = nations[i];
+            int nGoldCnt = nation.getGoldCnt();
+            int nSilverCnt = nation.getSilverCnt();
+            int nBronzeCnt = nation.getBronzeCnt();
 
-            if(nation2.getNationNo() == K) break;
+            if(targetGoldCnt < nGoldCnt) ++kRank;
+            else if(targetGoldCnt == nGoldCnt) {
+                if(targetSilverCnt < nSilverCnt) ++kRank;
+                else if(targetSilverCnt == nSilverCnt) {
+                    if(targetBronzeCnt < nBronzeCnt) ++kRank;
+                }
+            }
         }
 
-        System.out.print(tmpRank);
+        System.out.print(kRank);
     }
 
     public static class Nation {
@@ -76,10 +72,6 @@ public class Q8979 {
             this.bronzeCnt = bronzeCnt;
         }
 
-        public int getNationNo() {
-            return nationNo;
-        }
-
         public int getGoldCnt() {
             return goldCnt;
         }
@@ -90,11 +82,6 @@ public class Q8979 {
 
         public int getBronzeCnt() {
             return bronzeCnt;
-        }
-
-        public boolean 같은_등수인가(Nation nation) {
-            boolean 같은_등수_여부 = this.goldCnt == nation.getGoldCnt() && this.silverCnt == nation.getSilverCnt() && this.bronzeCnt == nation.getBronzeCnt();
-            return 같은_등수_여부;
         }
     }
 }
