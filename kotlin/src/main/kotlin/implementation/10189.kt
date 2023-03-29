e18d3e165ce12f7a1e879d0511231cd9f50daf4a
Jaewook Choi <eari100@naver.com>
2023-03-30 01:02:19 +0900

    [kotlin] Baekjoon, 10189. Hook, implementation

---
+++ kotlin/src/main/kotlin/implementation/10189.kt
@@ -0,0 +1,15 @@
+package implementation
+
+/**
+ * @source: https://www.acmicpc.net/problem/10189 (Hook)
+ * @classfication: implementation
+ * @문제 푼 날짜 (혼자 힘으로 풂?): 23.03.30 (O)
+ **/
+fun main() {
+    print("""
+        #  # #### #### #  #
+        #### #  # #  # # #
+        #### #  # #  # # #
+        #  # #### #### #  #
+    """.trimIndent())
+}
