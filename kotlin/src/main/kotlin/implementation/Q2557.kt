c0180d02014a96e6e123cafcb348d7867c590fdb
Jaewook Choi <eari100@naver.com>
2023-03-30 00:32:09 +0900

    [kotlin] Baekjoon, 2557. Hello World, implementation

---
+++ kotlin/src/main/kotlin/implementation/Q2557.kt
@@ -0,0 +1,10 @@
+package implementation
+
+/**
+ * @source: https://www.acmicpc.net/problem/2557 (Hello World)
+ * @classfication: implementation
+ * @문제 푼 날짜 (혼자 힘으로 풂?): 23.03.30 (O)
+ **/
+fun main() {
+    println("Hello World!")
+}
