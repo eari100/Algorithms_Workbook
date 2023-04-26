fbd7bd7918cbb3d277bc784cbd246ec9dd61ea7c
Jaewook <eari100@naver.com>
2023-04-26 18:18:31 +0900

    [Java] Baekjoon, 4539. 반올림
     - implementation
     - math

---
+++ Java/Baekjoon/implementation/Q4539.java
@@ -0,0 +1,41 @@
+package Baekjoon.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @source: https://www.acmicpc.net/problem/4539 (반올림)
+ * @classfication: implementation, math
+ * @문제 푼 날짜 (자력으로 풂?): 23.04.26 (O)
+ **/
+public class Q4539 {
+    public static void main(String[] args) throws IOException {
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder result = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            int digit = getDigit(x);
+
+            for(int i=1;i<digit;i++) {
+                x = getRound(x, i);
+            }
+
+            result.append(x).append('\n');
+        }
+
+        br.close();
+
+        System.out.print(result);
+    }
+
+    private static int getRound(int x, int digit) {
+        double y = Math.pow(10, digit) * 1.0;
+        return (int) (Math.round(x/y) * y);
+    }
+
+    private static int getDigit(int x) {
+        return (int) Math.log10(x) + 1;
+    }
+}
