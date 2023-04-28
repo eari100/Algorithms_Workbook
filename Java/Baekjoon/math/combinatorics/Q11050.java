798017760cb262b2503123aefd17aad1a94b08f7
Jaewook <eari100@naver.com>
2023-04-28 12:03:14 +0900

    chore: [Java] Baekjoon, 11050. 이항 계수 1, 참고하면 좋은 링크 추가

--- Java/Baekjoon/math/combinatorics/Q11050.java
+++ Java/Baekjoon/math/combinatorics/Q11050.java
@@ -9,6 +9,7 @@ import java.util.StringTokenizer;
  * @source: https://www.acmicpc.net/problem/11050 (이항 계수 1)
  * @classfication: combinatorics
  * @문제 푼 날짜 (자력으로 풂?): 23.04.28 (X, 조합 구하는 수식 공부했음)
+ * @reference: https://st-lab.tistory.com/159 (공부하면 좋은 링크)
  **/
 public class Q11050 {
     public static void main(String[] args) throws IOException {
