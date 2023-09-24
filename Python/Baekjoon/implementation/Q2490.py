e9fb5f35bd1a4a24643383026310a4eb62d5573a
Jaewook Choi <eari100@naver.com>
2023-09-25 00:39:11 +0900

    [Python] Baekjoon, 2490. 윷놀이, implementation, 코드 개선

--- Python/Baekjoon/implementation/Q2490.py
+++ Python/Baekjoon/implementation/Q2490.py
@@ -1,23 +1,4 @@
-answer = ''
+num_list = ['D', 'C', 'B', 'A', 'E']
 
 for _ in range(3):
-    num_list = list(map(int, input().split()))
-    x = 0
-    y = 0
-
-    for num in num_list:
-        if num == 0:
-            x += 1
-
-    if x == 1:
-        answer += 'A\n'
-    elif x == 2:
-        answer += 'B\n'
-    elif x == 3:
-        answer += 'C\n'
-    elif x == 4:
-        answer += 'D\n'
-    elif x == 0:
-        answer += 'E\n'
-
-print(answer)
+    print(num_list[sum(list(map(int, input().split())))])
