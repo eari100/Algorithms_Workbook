2a01d1b70b20727a2912c1fd9c24ecd57bb54fe2
Jaewook Choi <eari100@naver.com>
2023-10-09 19:05:39 +0900

    [Python] Programmers, 181852. 뒤에서 5등 위로, implementation

---
+++ Python/Programmers/implementation/Q181852.py
@@ -0,0 +1,5 @@
+# @source: https://school.programmers.co.kr/learn/courses/30/lessons/181852 (뒤에서 5등 위로)
+# @classification: implementation
+# @문제 푼 날짜 (자력으로 풂?): 23.10.09 (O)
+def solution(num_list):
+    return sorted(num_list)[5:]
