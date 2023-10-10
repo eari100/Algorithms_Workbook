4e7f9761997fbd5502f96c2e9e62d3420c37c639
Jaewook Choi <eari100@naver.com>
2023-10-11 00:50:08 +0900

    [Python] Programmers, 120854. 배열 원소의 길이, implementation

---
+++ Python/Programmers/implementation/Q120854.py
@@ -0,0 +1,6 @@
+# @source: https://school.programmers.co.kr/learn/courses/30/lessons/120854 (배열 원소의 길이)
+# @classification: implementation
+# @문제 푼 날짜 (자력으로 풂?): 23.10.11 (O)
+
+def solution(strlist):
+    return [len(str) for str in strlist]
