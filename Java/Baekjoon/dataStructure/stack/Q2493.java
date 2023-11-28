12a49f32436b2ae07f3032d22951af95aba197e3
Jaewook Choi <eari100@naver.com>
2023-11-28 20:21:21 +0900

    [Java] Baekjoon, 2493. 탑, stack

---
+++ Java/Baekjoon/dataStructure/stack/Q2493.java
@@ -0,0 +1,46 @@
+package Baekjoon.dataStructure.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * @source: https://www.acmicpc.net/problem/2493 (탑)
+ * @classfication: stack
+ * @문제 푼 날짜 (자력으로 풂?): 23.11.28 (X, 물체 구상 시 변형하는 사고를 가질 것)
+ **/
+public class Q2493 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Stack<int[]> stk = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        for(int i=1;i<=N;i++) {
+            int curr = Integer.parseInt(st.nextToken());
+
+            while( !stk.empty() ) {
+                int[] peek = stk.peek();
+                int prev = peek[1];
+
+                if(prev > curr) {
+                    result.append(peek[0]).append(' ');
+                    stk.add(new int[]{i, curr});
+                    break;
+                } else {
+                    stk.pop();
+                }
+            }
+
+            if(stk.isEmpty()) {
+                result.append(0).append(' ');
+                stk.add(new int[]{i, curr});
+            }
+        }
+
+        System.out.print(result);
+    }
+}
