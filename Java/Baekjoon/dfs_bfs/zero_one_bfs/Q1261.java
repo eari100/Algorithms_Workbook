8012f0a1e61923ac3e3165d6ca0616acc3c09963
Jaewook Choi <eari100@naver.com>
2023-11-29 22:15:47 +0900

    refactor: [Java] Baekjoon, 1261. 알고스팟, bfs -> 0-1 bfs (최적화)

--- Java/Baekjoon/dfs_bfs/zero_one_bfs/Q1261.java
+++ Java/Baekjoon/dfs_bfs/zero_one_bfs/Q1261.java
@@ -1,19 +1,16 @@
-package Baekjoon.dfs_bfs;
+package Baekjoon.dfs_bfs.zero_one_bfs;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * @source: https://www.acmicpc.net/problem/1261 (알고스팟)
  * @classification: bfs
  * @문제 푼 날짜 (자력으로 풂?): 23.11.29 (O)
  **/
-// todo: 0-1 bfs (최선), dijkstra (차선)으로 다시 풀어볼 것
+// todo: dijkstra 으로 다시 풀어볼 것
 public class Q1261 {
     final static int[] nX = {-1, 0, 1, 0};
     final static int[] nY = {0, 1, 0, -1};
@@ -40,17 +37,17 @@ public class Q1261 {
 
         brokenWallLog[0][0] = 0;
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0, 0});
+        ArrayDeque<int[]> deque = new ArrayDeque<>();
+        deque.offer(new int[]{0, 0, 0});
 
-        bfs(map, brokenWallLog, queue);
+        bfs(map, brokenWallLog, deque);
 
         System.out.print(brokenWallLog[m-1][n-1]);
     }
 
-    private static void bfs(int[][] map, int[][] brokenWallLog, Queue<int[]> queue) {
-        while( !queue.isEmpty() ) {
-            int[] poll = queue.poll();
+    private static void bfs(int[][] map, int[][] brokenWallLog, ArrayDeque<int[]> deque) {
+        while( !deque.isEmpty() ) {
+            int[] poll = deque.pollFirst();
             int x1 = poll[0];
             int y1 = poll[1];
             int wall1 = poll[2];
@@ -71,7 +68,11 @@ public class Q1261 {
 
                 brokenWallLog[x2][y2] = wall2;
 
-                queue.offer(new int[]{x2, y2, wall2});
+                if(map[x2][y2] == 0) {
+                    deque.addFirst(new int[]{x2, y2, wall2});
+                } else {
+                    deque.addLast(new int[]{x2, y2, wall2});
+                }
             }
         }
     }
