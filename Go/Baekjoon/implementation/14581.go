401d22c9acdad4f78cd3162e1d2c39a87a172fa1
Jaewook Choi <eari100@naver.com>
2024-06-10 00:48:43 +0900

    [Go] Baekjoon, 14581. 팬들에게 둘러싸인 홍준, implementation

---
+++ Go/Baekjoon/implementation/14581.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var id string
+	fmt.Scanln(&id)
+
+	fmt.Println(":fan::fan::fan:")
+	fmt.Printf(":fan::%s::fan:\n", id)
+	fmt.Println(":fan::fan::fan:")
+}
