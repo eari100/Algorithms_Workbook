9a655b0297cfce15936fc00e5679296ccecaba2f
Jaewook Choi <eari100@naver.com>
2024-08-17 02:34:57 +0900

    [C] Baekjoon, 1809. Moo, implementation

---
+++ C/Q1809.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//
+//int main(void) {
+//    const char *cow = 
+//        " (___)\n"
+//        " (o o)____/\n"
+//        "  @@      \\\n"
+//        "   \\ ____,/\n"
+//        "   //   //\n"
+//        "  ^^   ^^\n";
+//
+//    printf("%s", cow);
+//
+//    return 0;
+//}
