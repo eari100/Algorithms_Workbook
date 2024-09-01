ce5fa4b95724605e1ea9c594cff7dbe53a16e542
Jaewook Choi <eari100@naver.com>
2024-09-01 17:51:55 +0900

    [Python] Beakjoon, 2721. 삼각수의 합, 성능 향상

--- Python/Baekjoon/dp/Q2721.py
+++ Python/Baekjoon/dp/Q2721.py
@@ -3,16 +3,16 @@
 # @문제 푼 날짜 (자력솔): 24.09.01 (O)
 t = int(input())
 answer = []
+dp1 = [1]
+dp2 = [0]
 
 for _ in range(t):
     n = int(input())
-    dp1 = [0] * (n + 1)
-    dp1[0] = 1
-    dp2 = [0] * (n + 1)
 
     for i in range(1, n+1):
-        dp1[i] = dp1[i-1] + i + 1
-        dp2[i] = dp2[i-1] + (i * dp1[i])
+        if len(dp1) < i+1:
+            dp1.append(dp1[i-1] + i + 1)
+            dp2.append(dp2[i-1] + (i * dp1[i]))
 
     answer.append(str(dp2[n]))
 
