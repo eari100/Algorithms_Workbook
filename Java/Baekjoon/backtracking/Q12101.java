0327df83a1113955713e895269fadf17f8ab6430
Jaewook Choi <eari100@naver.com>
2024-11-14 23:05:44 +0900

    [Java] Baekjoon, 12101. 1, 2, 3 더하기 2, backtracking

---
+++ Java/Baekjoon/backtracking/Q12101.java
@@ -0,0 +1,48 @@
+package Baekjoon.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @source: https://www.acmicpc.net/problem/12101 (1, 2, 3 더하기 2)
+ * @classfication: backtracking
+ * @문제 푼 날짜 (자력으로 풂?): 24.11.14 (O)
+ **/
+public class Q12101 {
+    static int n, k;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        br.close();
+
+        bt("", 0);
+
+        System.out.print(-1);
+    }
+
+    private static void bt(String memo, int sum) {
+        if(sum == n) {
+            --k;
+
+            if(k == 0) {
+                System.out.print(memo);
+                System.exit(0);
+            }
+
+            return;
+        }
+
+        for(int i=1;i<=3;i++) {
+            int sum2 = sum + i;
+
+            if(sum2 > n) break;
+
+            bt(memo.equals("") ? i+"" : memo+"+"+i, sum2);
+        }
+    }
+}
