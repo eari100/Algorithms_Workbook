ba5dd5276d639b295511da61357d0bb18a21bb0f
Jaewook Choi <eari100@naver.com>
2025-07-28 03:13:41 +0900

    [C++] Baekjoon, 2161. 카드1, implementation

---
+++ cpp/Baekjoon/implementation/Q2161.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  queue<int> que;
+
+  for(int i=1;i<=n;i++)
+  {
+    que.push(i);
+  }
+
+  while(que.size() > 1)
+  {
+    cout << que.front() << " ";
+    que.pop();
+
+    int f = que.front();
+    que.pop();
+    que.push(f);
+  }
+
+  cout << que.front();
+}
