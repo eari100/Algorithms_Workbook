e31cf20462e5a579aeee86ad842366a6e89711a8
Jaewook Choi <eari100@naver.com>
2025-11-17 10:26:13 +0900

    [C++] Baekjoon, 32458. Just Round Down, implementation

---
+++ cpp/Baekjoon/implementation/Q32458.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+/**
+ * @source: https://www.acmicpc.net/problem/32458 (Just Round Down)
+ * @classification: implementation
+ * @문제 푼 날짜 (자력으로 풂?): 25.11.17 (O)
+ **/
+double d;
+
+int main()
+{
+    cin>>d;
+    cout << int(d);
+}
